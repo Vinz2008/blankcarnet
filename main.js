664e0891a92452de1402289fe452426e47bac071
Vinz2008 <clubfunreading@gmail.com>
2021-12-05 14:47:23 +0100

    added js

---
+++ main.js
@@ -0,0 +1,15 @@
+var textarea = document.getElementById("content");
+$(function () {
+    var combination = ''
+    key('f2', function(){ 
+       console.log("test1")
+    });
+    
+
+ });
+/*
+if(textarea.selectionStart == textarea.selectionEnd) {
+    
+
+};
+*/
