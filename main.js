bb493130b55203b76e9737d4e40fae057c1e16e2
Vinz2008 <clubfunreading@gmail.com>
2021-12-05 14:54:44 +0100

    fixed heading

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 var textarea = document.getElementById("content");
+
+
+
 $(function () {
-    var combination = ''
-    key('f2', function(){ 
-       console.log("test1")
-    });
-    
+   var combination = ''
+   key('f2', function(){ 
+      console.log("test1")
+   });
+      
+  
+});
 
- });
-/*
-if(textarea.selectionStart == textarea.selectionEnd) {
-    
 
-};
-*/
